b077cf8c4b3ebffd751624fb3d7e54a6ead72ecf
Debjit Mandal <debjitmandal8925@gmail.com>
2024-06-10 21:51:07 +0530

    fix: Toggle button position fixed

--- scripts/theme.js
+++ scripts/theme.js
@@ -1,19 +1,20 @@
 document.addEventListener('DOMContentLoaded', () => {
-    const themeToggle = document.getElementById('theme-toggle');
+    const checkbox = document.getElementById('checkbox');
     const body = document.body;
     const currentTheme = localStorage.getItem('theme');
 
     if (currentTheme) {
         body.classList.toggle('dark-mode', currentTheme === 'dark');
         body.classList.toggle('light-mode', currentTheme === 'light');
-        themeToggle.innerHTML = currentTheme === 'dark' ? '<i class="fas fa-moon"></i>' : '<i class="fas fa-sun"></i>';
+        checkbox.checked = currentTheme === 'light';
+    } else {
+        localStorage.setItem('theme', 'dark');
     }
 
-    themeToggle.addEventListener('click', () => {
+    checkbox.addEventListener('change', () => {
         body.classList.toggle('dark-mode');
         body.classList.toggle('light-mode');
         const newTheme = body.classList.contains('dark-mode') ? 'dark' : 'light';
         localStorage.setItem('theme', newTheme);
-        themeToggle.innerHTML = newTheme === 'dark' ? '<i class="fas fa-moon"></i>' : '<i class="fas fa-sun"></i>';
     });
 });
